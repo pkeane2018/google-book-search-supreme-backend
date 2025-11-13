f8895192195162655c56a652de9b18623e8bc746
pkeane2018 <pkeane92@yahoo.com>
2025-11-13 14:50:23 -0600

    created controller and service classes with functions for get all saved books endpoint work in progress

---
+++ src/main/java/com/herokuapp/google_book_search_supreme_backend/controller/BookController.java
@@ -0,0 +1,24 @@
+package com.herokuapp.google_book_search_supreme_backend.controller;
+
+import com.herokuapp.google_book_search_supreme_backend.entity.Book;
+import com.herokuapp.google_book_search_supreme_backend.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/books")
+public class BookController {
+
+    @Autowired
+    BookService service;
+
+    @GetMapping
+    public List<Book> getAllSavedBooks() {
+        System.out.println("Hoochy Mama!");
+        return service.getAllSavedBooks();
+    }
+}
